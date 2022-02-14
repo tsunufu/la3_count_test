304a4bc46eb4861955eac4b5d8af03df9a2e8345
tsunufu <tsunufu@icloud.com>
2022-02-14 05:46:10 +0000

    Implement feature: creating new counter

--- app.rb
+++ app.rb
@@ -8,3 +8,44 @@ before do
     Count.create(number: 0)
   end
 end
+
+get '/' do
+  @number = Count.first.number
+  @number2 = 0
+  erb :index
+end
+
+post '/plus' do
+  count = Count.first
+  count.number = count.number + 1
+  count.save
+  redirect '/'
+end
+
+post '/minus' do
+  count = Count.first
+  count.number = count.number - 1
+  count.save
+  redirect '/'
+end
+
+post '/kake' do
+  count = Count.first
+  count.number = count.number * 2
+  count.save
+  redirect '/'
+end
+
+post '/wari' do
+  count = Count.first
+  count.number = count.number / 2
+  count.save
+  redirect '/'
+end  
+  
+post '/clear' do
+  count = Count.first
+  count.number = 0
+  count.save
+  redirect '/'
+end
--- db/migrate/20200323082248_create_counts.rb
+++ db/migrate/20200323082248_create_counts.rb
@@ -2,6 +2,7 @@ class CreateCounts < ActiveRecord::Migration[6.1]
   def change
     create_table :counts do |t|
       t.integer :number
+      t.integer :number2
       t.timestamps null: false
     end
   end
